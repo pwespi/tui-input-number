0b547fa75e9a39a9d7e1fc59cbc2bc032a645900
Patrick Wespi <patrick.wespi@positrigo.com>
2022-09-02 16:36:44 +0200

    add tui-input-number and provide tui number format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  TuiDecimalSymbol,
+  TuiNumberFormatSettings,
+  TUI_NUMBER_FORMAT,
+} from '@taiga-ui/core';
+
+export class TuiNumberFormat implements TuiNumberFormatSettings {
+  get decimalLimit(): number {
+    return Infinity;
+  }
+
+  get decimalSeparator(): TuiDecimalSymbol {
+    return '.';
+  }
+
+  get thousandSeparator(): string {
+    return "'";
+  }
+
+  get zeroPadding(): boolean {
+    return false;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    // this works:
+    // {
+    //   provide: TUI_NUMBER_FORMAT,
+    //   useValue: {
+    //     decimalLimit: Infinity,
+    //     decimalSeparator: '.',
+    //     thousandSeparator: "'",
+    //     zeroPadding: false,
+    //   },
+    // },
+
+    // this causes problems:
+    {
+      provide: TUI_NUMBER_FORMAT,
+      useClass: TuiNumberFormat,
+    },
+  ],
 })
 export class AppComponent {
-  title = 'tui-input-number';
+  readonly control = new FormControl(null);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { TuiRootModule } from "@taiga-ui/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule } from '@taiga-ui/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TuiInputNumberModule } from '@taiga-ui/kit';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-      BrowserAnimationsModule,
-      TuiRootModule
-],
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TuiRootModule,
+    TuiInputNumberModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
